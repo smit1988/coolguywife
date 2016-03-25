f7d2715f1ac583a4f00555549bfd242dadd15a90
smit1988 <smit1988@purdue.edu>
2016-03-25 01:59:58 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function respond() {
       botRegexExams = /^\/exams/;  botRegexKiss = /^\/kiss/;  botRegexBurn = /^\/burn/;  botRegex = /^\/cool guy/;  botRegexBall = /^\/8ball/;  botRegexSpook = /^\/spooky/;  botRegexDie = /^\/roll a die/;  botRegexMeme = /^\/meme/; botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexRules = /^\/rules/
       botRegexReddit = /^\/reddit/;  botRegexPraise = /^\/praise cool guy/;  botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
-      botRegexSiege = /^\/siege/;
+      botRegexSiege = /^\/siege/;  botRegexStock = /^\/stock/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -120,6 +120,14 @@ function respond() {
       return Math.floor(Math.random() * 10);
     }
     postMessage("http://www.memecreator.org/static/images/memes/38"+rand()+rand()+rand()+rand()+rand()+".jpg");
+    this.res.end();
+  }
+  else if(request.text && botRegexStock.test(request.text)) {
+    this.res.writeHead(200);
+    function rand(){
+      return Math.floor(Math.random() * 10);
+    }
+    postMessage("http://comps.canstockphoto.com/can-stock-photo_csp"+Math.floor(Math.random() * 8)+rand()+rand()+rand()+rand()+rand()+rand()+".jpg");
     this.res.end();
   }
   else if(request.text && botRegexDie.test(request.text)) {
