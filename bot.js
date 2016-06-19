1e3bd359a1b9803205e57104c628d19fcd405990
smit1988 <smit1988@purdue.edu>
2016-06-19 02:24:10 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -197,7 +197,7 @@ function respond() {
   }
   else if(request.text && botRegexDie.test(request.text)) {
     this.res.writeHead(200);
-    var req = request.text.substring(5,request.text.length);
+    var req = request.text.substring(6,request.text.length);
     req = req[0].toUpperCase() + req.slice(1);
     postMessage(req + " was rolled");
     this.res.end();
