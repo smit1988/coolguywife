3c0a86518ed49fb16d684392305045a88263bad9
smit1988 <smit1988@purdue.edu>
2016-06-19 02:22:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -198,8 +198,8 @@ function respond() {
   else if(request.text && botRegexDie.test(request.text)) {
     this.res.writeHead(200);
     var req = request.text.substring(5,request.text.length);
-    req = req.charAt(0).toUpperCase() + req.slice(1);
-    postMessage(req + "was rolled");
+    req = req[0].toUpperCase() + req.slice(1);
+    postMessage(req + " was rolled");
     this.res.end();
   }
   else if(request.text && botRegexCoin.test(request.text)) {
